a4eab403555e53a0960a110fccf4b6a5b86338f5
B0G0N1 <michelebogoni2001@gmail.com>
2024-09-06 01:22:53 +0200

    [FEAT] Create input Form

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP-Badwards</title>
+    <title>Form di Censura</title>
 </head>
 <body>
-    
+    <h1>Inserisci il paragrafo e la parola da censurare</h1>
+    <form action="censorship.php" method="POST">
+        <label for="paragraph">Paragrafo:</label><br>
+        <textarea name="paragraph" id="paragraph" rows="5" cols="40" required></textarea><br><br>
+        
+        <label for="word">Parola da censurare:</label><br>
+        <input type="text" name="word" id="word" required><br><br>
+        
+        <button type="submit">Invia</button>
+    </form>
 </body>
 </html>
